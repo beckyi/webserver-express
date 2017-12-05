8994b8ea39a95385bdbd9dac03af2985114b974c
ungmo2 <ungmo2@gmail.com>
2017-12-05 10:44:15 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+/*  DEPENDENCIES */
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+/*******************************
+  Endpoint
+*******************************/
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => console.log(`Server listening on port ${port}`));
